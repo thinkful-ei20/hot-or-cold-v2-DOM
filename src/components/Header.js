84e86fb3e673c340d7da5573403b246a9fd1e14d
mckoyd <dominick.mckoy@gmail.com>
2018-05-19 13:56:51 -0400

    Finished header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import NavBar from './NavBar';
+import '../styles/header.css'
 
 export default () => {
   return(
     <header>
         <NavBar />
+        <h1>HOT or COLD</h1>
     </header>
   )
 }
