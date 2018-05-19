73b1b43e5dcc257c62ab8573526f2db7b286fa67
mckoyd <dominick.mckoy@gmail.com>
2018-05-19 14:33:19 -0400

    Hot or Cold static page complete

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import GameForm from './GameForm';
+import GameResults from './GameResults';
+import GameGuess from './GameGuess';
+
 import '../styles/game.css';
 
 export default () => {
@@ -7,6 +10,8 @@ export default () => {
     <section className='game'>
       <h2 id='feeback'>Make Your Guess!</h2>
       <GameForm />
+      <GameGuess />
+      <GameResults />
     </section>
   );
 }
---
+++ src/components/GameResults.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <ul id="guessList" class="guessBox clearfix">
+    </ul>
+  )
+}
