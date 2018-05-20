4747f09a9bec378fcf606d2bce9dcf1ce3999a8a
mckoyd <dominick.mckoy@gmail.com>
2018-05-20 14:21:00 -0400

    Now form disappears upon winning game

--- src/components/GameForm.js
+++ src/components/GameForm.js
@@ -7,32 +7,41 @@ import {getGuess} from '../actions/index';
 export class GameForm extends React.Component {
   onGuessing(event) {
     event.preventDefault();
-    this.props.dispatch(getGuess(this.userGuess));
-    this.userGuess = '';
+    this.props.dispatch(getGuess(this.userGuess)); this.userGuess = '';
     document.getElementById('userGuess').value = '';
     document.getElementById('userGuess').focus();
   }
   render() {
+    const display = {
+      display: this.props.feedback==='You got it!' ? 'none' : 'block'
+    }
     return(
       <form onSubmit={e => this.onGuessing(e)}>
-        <input  type='text' 
+        <input  type='number' 
                 name='userGuess' 
                 id='userGuess' 
                 className='text' 
-                maxLength='3' 
+                min='1'
+                max='100'
                 autoComplete='off' 
                 placeholder='Enter your Guess' 
                 required
-                onChange={event => (this.userGuess = event.target.value)} />
+                onChange={event => (this.userGuess = event.target.value)}
+                style={display} />
         <input  type='submit' 
                 id='guessButton' 
                 className='button' 
                 name='submit' 
-                value='Guess' />
+                value='Guess'
+                style={display} />
       </form>
     );
   }
   
 }
 
-export default connect()(GameForm);
+const mapStateToProps = state => ({
+  feedback: state.feedback
+});
+
+export default connect(mapStateToProps)(GameForm);
